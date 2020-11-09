5e689c06a35eb192c50f370c5d664d32862f38b7
Danny McNaught <danny.mcnaught@dannymcnaught.com>
2020-11-08 19:31:06 -0500

    Upload xml changes to orgin/master

--- WPFAppWithDocFxAndLogging2/WPFAppWithDocFxAndLogging2/MainWindow.xaml.cs
+++ WPFAppWithDocFxAndLogging2/WPFAppWithDocFxAndLogging2/MainWindow.xaml.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿//***********************************************************************
+// Assembly         : WPFAppWithDocFxAndLogging2
+// Author UserID    : danny.mcnaught
+// Author Full Name : Danny C. McNaught
+// Author Phone     : 1-919-239-3306
+// Created          : 11-08-2020
+//
+// Created By       : Danny C. McNaught
+// Last Modified By : Danny C. McNaught
+// Last Modified On : 11-08-2020
+// Change Request # :
+// Version Number   :
+// Description      :
+// File Name        : MainWindow.xaml.cs
+// Copyright        : (c) Computer Question. All rights reserved.
+// <summary></summary>
+// ***********************************************************************
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,25 +28,73 @@ using System.Windows;
 
 namespace WPFAppWithDocFxAndLogging2
 {
-    /// <summary>
-    ///    Class MainWindow
-    /// </summary>
+    /// <summary>Class MainWindow</summary>
+    /// <remarks>
+    /// <para><b>History:</b></para>
+    /// <list type="table">
+    /// <item>
+    /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+    /// </item>
+    /// <item>
+    /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+    /// </item>
+    /// <item>
+    /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+    /// </item>
+    /// </list>
+    /// </remarks>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Gets or sets the factory.
-        /// </summary>
+        /// <summary>Gets or sets the factory.</summary>
         /// <value>The factory.</value>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public static ILoggerFactory Factory { get; set; }
-        /// <summary>
-        /// Gets or sets the logger.
-        /// </summary>
+        /// <summary>Gets or sets the logger.</summary>
         /// <value>The logger.</value>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public static ILogger Logger { get; set; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:System.Windows.Window" /> class.
-        /// </summary>
+        /// <summary>Initializes a new instance of the <see cref="T:System.Windows.Window" /> class.</summary>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public MainWindow()
         {
             InitializeComponent();
@@ -48,27 +113,63 @@ namespace WPFAppWithDocFxAndLogging2
             Logger.LogInformation(message: $"{ShowDebugInfo()}Created Log File for {logFileName}.");
 
         }
-        /// <summary>
-        /// Applications the path.
-        /// </summary>
+        /// <summary>Applications the path.</summary>
         /// <returns>System.String.</returns>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public static string AppPath()
         {
             return Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
         }
 
-        /// <summary>
-        /// Applications the name.
-        /// </summary>
+        /// <summary>Applications the name.</summary>
         /// <returns>System.String.</returns>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:30 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public static string AppName()
         {
             return Assembly.GetExecutingAssembly().GetName().Name;
         }
-        /// <summary>
-        /// Shows the debug information scan form.
-        /// </summary>
+        /// <summary>Shows the debug information. Uploaded to orgin/master.</summary>
         /// <returns>System.String.</returns>
+        /// <remarks>
+        /// <para><b>History:</b></para>
+        /// <list type="table">
+        /// <item>
+        /// <description><b>Code Changed by:</b><para>Danny C. McNaught</para><para><para><a href="mailto:danny.mcnaught@dannymcnaught.com">danny.mcnaught@dannymcnaught.com</a></para></para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code changed on Visual Studio Host Machine:</b><para>DESKTOP-ACLFE3O</para></description>
+        /// </item>
+        /// <item>
+        /// <description><b>Code Change Date and Time:</b><para>Sunday, November 8, 2020 7:29 PM</para><b>Code Changes:</b><para>Created XML Comment</para></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public static string ShowDebugInfo()
         {
             try
